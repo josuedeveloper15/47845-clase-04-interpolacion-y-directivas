d0d5d3df15a79a9ca90084291a14f2857eba0ecd
josuebaez61@gmail.com <josuebaez61@gmail.com>
2023-09-20 21:02:10 -0300

    ADD, directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'clase-04-interpolacion-y-directivas';
+
+  enabled = false;
+
+  getBackgroundColor(): object {
+    return {
+      backgroundColor: this.enabled ? 'green' : 'red',
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RoundedBlockDirective } from './directives/rounded-block.directive';
+import { DraggableDirective } from './directives/draggable.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, RoundedBlockDirective, DraggableDirective],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
